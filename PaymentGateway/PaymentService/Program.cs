bd2c34a18cd0479e91f022430f638db4dd74e093
Mihaela P <petre.mihaela8@gmail.com>
2019-05-27 14:43:31 +0100

    [PaymentGateway] - Add Azure Application Insights

--- PaymentGateway/PaymentService/Program.cs
+++ PaymentGateway/PaymentService/Program.cs
@@ -12,6 +12,7 @@ namespace PaymentService
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
